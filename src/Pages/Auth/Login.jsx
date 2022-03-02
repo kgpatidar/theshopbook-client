baf3328821738fccfaabafb47a3062debf8ac7f6
Krishna Gopal Patidar <krishnagopalpersonal@gmail.com>
2022-03-02 23:55:09 +0530

    Login Layout

--- src/Pages/Auth/Login.jsx
+++ src/Pages/Auth/Login.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="w-screen h-screen flex">
+      <div className="w-1/4 h-screen bg-white"></div>
+      <div className="w-3/4 h-screen bg-blue-300"></div>
+    </div>
+  );
 };
 
 export default Login;
